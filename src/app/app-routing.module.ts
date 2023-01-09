c7a2144f1adaf164aba6d03d88655fe0a6d5c4e7
ViniciusIth <vini.ithalo123@gmail.com>
2023-01-09 18:12:40 -0300

    Created shared module and search bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'admin',
-    loadChildren: () =>
-      import('./admin/admin.module').then((m) => m.AdminModule),
-  },
-
   // If no match redirect home
   { path: '**', redirectTo: '' }
 ];
---
+++ src/app/shared/search-bar/search-bar.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SearchBarComponent {
+  searchBar = new FormControl('');
+}
