2a00d26036e2b87530336f6bb37dc367e346be73
ViniciusIth <vini.ithalo123@gmail.com>
2023-01-09 20:03:32 -0300

    Started transition to decentralized layout schema
    - Created Admin module
    
    - Created Auth module
    
    - Created public module
    
    - Created User module
    
    - Each of the four layout modules is loaded lazily.
    
    The method allows for usage of multiples layout components instead of
    `ngif` directives.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+  },
+  {
+    path: 'p',
+    loadChildren: () =>
+      import('./public/public.module').then((m) => m.PublicModule),
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then((m) => m.UserModule),
+  },
   // If no match redirect home
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
