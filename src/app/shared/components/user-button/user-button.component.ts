eb8430e28b242f6e4b8e9389576282a4640d58cd
ViniciusIth <vini.ithalo123@gmail.com>
2023-01-11 18:09:13 -0300

    Added user button component

---
+++ src/app/shared/components/user-button/user-button.component.ts
@@ -0,0 +1,12 @@
+import { AuthService } from './../../../core/services/auth.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'shr-user-button',
+  templateUrl: './user-button.component.html',
+  styleUrls: ['./user-button.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserButtonComponent {
+  constructor(private auth: AuthService) {}
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { UserButtonComponent } from './components/user-button/user-button.component';
 
 @NgModule({
-  declarations: [SearchBarComponent, HeaderComponent, FooterComponent],
+  declarations: [SearchBarComponent, HeaderComponent, FooterComponent, UserButtonComponent],
   imports: [CommonModule, ReactiveFormsModule],
   exports: [SearchBarComponent, HeaderComponent, FooterComponent],
 })
