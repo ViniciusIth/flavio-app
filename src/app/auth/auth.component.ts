027d4a3d4a8fca95764ab911d12467113a97bff3
ViniciusIth <vini.ithalo123@gmail.com>
2023-01-13 00:29:42 -0300

    Added auth pages

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,29 @@
+import {
+  trigger,
+  transition,
+  query,
+  style,
+  stagger,
+  animate,
+} from '@angular/animations';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'sec-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
+  animations: [
+    trigger('toggleTrigger', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
-export class AuthComponent {}
+export class AuthComponent {
+  state: 'login' | 'register' = 'login';
+
+  alternateState() {
+    this.state = this.state == 'login' ? 'register' : 'login';
+  }
+}
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,15 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { AuthComponent } from './auth.component';
-
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
-  declarations: [
-    AuthComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthRoutingModule
-  ]
+  declarations: [AuthComponent, LoginComponent, RegisterComponent],
+  imports: [CommonModule, AuthRoutingModule],
 })
-export class AuthModule { }
+export class AuthModule {}
