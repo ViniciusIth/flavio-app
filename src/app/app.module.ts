0cdcf01937108f2769f09716a82681c055d72563
ViniciusIth <vini.ithalo123@gmail.com>
2023-01-13 00:28:18 -0300

    Added browser animation module and router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    RouterModule,
     CommonModule,
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,11 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
 import { UserButtonComponent } from './components/user-button/user-button.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SearchBarComponent, HeaderComponent, FooterComponent, UserButtonComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [RouterModule, CommonModule, ReactiveFormsModule],
   exports: [SearchBarComponent, HeaderComponent, FooterComponent],
 })
 export class SharedModule {}
