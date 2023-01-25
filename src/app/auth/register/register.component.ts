b3e39892bf9ad921fd8e978117ce2a74a1b10399
ViniciusIth <vini.ithalo123@gmail.com>
2023-01-25 16:04:37 -0300

    Finished auth page style

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,11 +1,41 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 
 @Component({
   selector: 'auth-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
 
+    email: new FormControl('', [Validators.required, Validators.email]),
+
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        '^(?=[^A-Z]*[A-Z])(?=[^a-z]*[a-z])(?=[^0-9]*[0-9]).{6,}$'
+      ),
+      Validators.minLength(8),
+    ]),
+
+    passwordConfirm: new FormControl('', [
+      Validators.required,
+      (control: FormControl): ValidationErrors | null => {
+        let pass = control.root.get('password')?.value;
+        let confirmPass = control.root.get('passwordConfirm')?.value;
+        console.log(control, pass, confirmPass);
+
+        return pass === confirmPass ? null : { notSame: true };
+      },
+    ]),
+  });
 }
