ef67bb7591bad22768bfdf9f466c15fd3c3b81d3
ViniciusIth <vini.ithalo123@gmail.com>
2023-02-07 18:05:11 -0300

    Added `getAuthData` and `saveAuthData`

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,3 +1,4 @@
+import { UserAuth } from './../interfaces/user-auth.schema';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
@@ -26,7 +27,22 @@ export class AuthService {
   }
 
   isLoggedIn(): boolean {
-    const access_token = localStorage.getItem('token')
-    return access_token !== null && access_token.length > 0;
+    const access_token = this.getAuthData()?.access_token
+
+    return access_token !== undefined && access_token.length > 0;
+
+  }
+
+  saveAuthData(authData: UserAuth) {
+    localStorage.setItem('auth', JSON.stringify(authData));
+  }
+
+  getAuthData(): UserAuth | null {
+    const authData = localStorage.getItem('auth');
+
+    if (authData) {
+      return JSON.parse(authData);
+    }
+    return null;
   }
 }
