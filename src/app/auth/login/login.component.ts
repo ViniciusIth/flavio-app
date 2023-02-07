ec4f3987a1a640114f11541b5b4552ab360878cc
ViniciusIth <vini.ithalo123@gmail.com>
2023-02-07 18:04:39 -0300

    Login redirects to users page on success

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,10 @@
+import { User } from './../../core/interfaces/user.schema';
+import { UserAuth } from './../../core/interfaces/user-auth.schema';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
+import { catchError, of, Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'auth-login',
@@ -9,7 +13,10 @@ import { AuthService } from 'src/app/core/services/auth.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginComponent {
-  constructor(private auth: AuthService) {}
+  constructor(
+    private route: Router,
+    private auth: AuthService
+    ) {}
 
   hasAttempts = false;
 
@@ -23,7 +30,18 @@ export class LoginComponent {
     if (formValues.email && formValues.password) {
       this.auth
         .login(formValues.email, formValues.password)
-        .subscribe((data) => {});
+        .pipe(
+          catchError((err, caught) => {
+            console.log(err, caught);
+            return of();
+          })
+        )
+        .subscribe((data: Partial<User>) => {
+          if (data.auth) {
+            this.auth.saveAuthData(data.auth);
+            this.route.navigate(['user'])
+          }
+        });
     }
   }
 }
